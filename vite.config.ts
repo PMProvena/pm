6683293c5f88f85766801122e0085277cfc59e1a
misspelz <adetoyeofficial@gmail.com>
2025-11-11 11:42:43 +0100

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "./", // important for Vercel static deploy
+  base: "./",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
